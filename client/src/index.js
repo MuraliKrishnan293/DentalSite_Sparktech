ecac84f93db72554827037938c8751cf87795ff6
MuraliKrishnan <muralikrishnan.cse293@gmail.com>
2024-12-06 01:15:20 +0530

    Console Bugs

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,13 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from '../src/Components/Redux/store';
 
+
+if (process.env.NODE_ENV === 'production') {
+  console.log = function() {};
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
